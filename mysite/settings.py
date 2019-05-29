4d26f104693fe4b82014ff6c481794c2d335ef0f
Theo Fountain III <Theoiiifountain@gmail.com>
2019-05-29 15:27:52 -0400

    Fix ALLOWED_HOSTS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['courtcasemanagement.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['courtcasemanagement.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
