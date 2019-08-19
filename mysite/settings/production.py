fc8414ae5c801bb18ef98722292d3d1b24b94fe8
Theo Fountain III <Theoiiifountain@gmail.com>
2019-08-19 13:34:02 -0400

    Add psycopg binary to requirements.txt

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,9 +1,10 @@
+import dj_database_url
+
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['courtcasemanagement.herokuapp.com', ]
 
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
