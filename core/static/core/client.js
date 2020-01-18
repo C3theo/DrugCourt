b9be1a42471450ca5e84bd08fc52a44012c3e5ff
Theo Fountain III <Theoiiifountain@gmail.com>
2020-01-17 21:21:55 -0500

    Add court and intake AJAX templates.

---
+++ core/static/core/client.js
@@ -0,0 +1,66 @@
+$(function () {
+
+  var loadForm = function () {
+    var btn = $(this);
+    $.ajax({
+      url: btn.attr("data-url"),
+      type: 'GET',
+      dataType: 'json',
+      beforeSend: function () {
+        $("#modal-client .modal-content").html("");
+        $("#modal-client").modal("show");
+      },
+      success: function (data) {
+        $("#modal-client .modal-content").html(data.html_form);
+      }
+    });
+  };
+
+  var saveForm = function () {
+    var form = $(this);
+    $.ajax({
+      url: form.attr("action"),
+      data: form.serialize(),
+      type: form.attr("method"),
+      dataType: 'json',
+      success: function (data) {
+        if (data.form_is_valid) {
+          $("#client-table tbody").html(data.html_client_list);
+          $("#modal-client").modal("hide");
+        }
+        else {
+          $("#modal-client .modal-client").html(data.html_form);
+        }
+      }
+    });
+    return false;
+  };
+
+  // Create client
+  $(".js-create-client").click(loadForm);
+  $("#modal-client").on("submit", ".js-client-create-form", saveForm);
+
+  // Update client
+  $("#client-table").on("click", ".js-update-client", loadForm);
+  $("#modal-client").on("submit", ".js-client-update-form", saveForm);
+
+  // Delete client
+  $("#client-table").on("click", ".js-delete-client", loadForm);
+  $("#modal-client").on("submit", ".js-client-delete-form", saveForm);
+
+  // This is working
+  // $(".js-create-court").click(function () {
+  //   $.ajax({
+  //     url: 'create',
+  //     type: 'get',
+  //     dataType: 'json',
+  //     beforeSend: function () {
+  //       $("#modal-court").modal("show");
+  //     },
+  //     success: function (data) {
+  //       $("#modal-court .modal-content").html(data.html_form);
+  //     }
+  //   });
+  // });
+
+});
